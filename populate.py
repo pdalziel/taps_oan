ccbf408981017d47d6a50cc8f41ff561332cc3c0
kolox- <colinbutler1995@gmail.com>
2017-02-23 21:24:35 +0000

    Added preliminary random populate script

---
+++ populate.py
@@ -0,0 +1,91 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'project.settings')
+
+import django
+import random
+django.setup()
+from taps_oan.models import Beer, Pub
+
+def populate():
+    # First, we will create lists of dictionaries containing the beers
+    # we want to add into each category.
+    # Then we will create a dictionary of dictionaries for our categories.
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+
+    arbitrary_beers = [
+        {"title": "Joker IPA",
+         "views": "1"},
+        {"title": "Tennents",
+         "views": "2"},
+        {"title": "Coors Light",
+         "views": "4"},
+        {"title": "West",
+         "views": "8"},
+        {"title": "Guiness",
+         "views": "10"},
+        {"title": "Punk IPA",
+         "views": "13"},
+        {"title": "Carling",
+         "views": "17"},
+        {"title": "Carlsberg",
+         "views": "22"},
+        {"title": "Heineken",
+         "views": "26"},
+        {"title": "Staropramen",
+         "views": "29"},
+        {"title": "Brooklyn Lager",
+         "views": "33"},
+        {"title": "John Smith",
+         "views": "41"},
+        {"title": "Strongbow",
+         "views": "54"} ]
+
+    arbitrary_pubs = [
+        {"name": "GUU",
+         "views": "1"},
+        {"name": "QMU",
+         "views": "2"},
+        {"name": "Bar Soba",
+         "views": "4"},
+        {"name": "Oran Mor",
+         "views": "8"},
+        {"name": "The Crafty Pig",
+         "views": "12"},
+        {"name": "Coopers",
+         "views": "17"},
+        {"name": "Hillhead Bookclub",
+         "views": "23"} ]
+
+
+    for beer in arbitrary_beers:
+        add_beer(beer["title"], beer["views"])
+
+    for pub in arbitrary_pubs:
+        p = add_pub(pub["name"], pub["views"])
+        for b in Beer.objects.all():
+            if (random.randint(0,10)<5):
+                p.beers.add(b)
+                
+    for check_pub in Pub.objects.all():
+        for check_beer in check_pub.beers.all():
+            print("- {0} - {1}".format(str(check_pub), str(check_beer)))
+            
+        
+    
+def add_pub(name, views):
+    p = Pub.objects.get_or_create(name=name)[0]
+    p.views = views
+    p.save()
+    return p
+
+def add_beer(title, views):
+    b = Beer.objects.get_or_create(title=title)[0]
+    b.views = views
+    b.save()
+    return b
+
+
+if __name__ == '__main__':
+    populate()
