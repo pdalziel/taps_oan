224df74fa75c56b47fd18f78dc268709fd8e6c13
pdalziel <mail@pdalziel.xyz>
2017-03-01 17:32:10 +0000

    edited registration templates to remove profile

--- taps_oan/forms.py
+++ taps_oan/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from taps_oan.models import Pub, Beer, UserProfile
+
 
 class PubForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the pub name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = forms.CharField(widget=forms.HiddenInput(), required=False) 
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         # Provide an association between the ModelForm and a model
         model = Pub
         fields = ('name',)
+
 
 """ Not used anymore - always threw an integrity issue when adding adding
     a beer that already existed.
@@ -28,28 +30,32 @@ class BeerForm(forms.ModelForm):
         exclude = ('pub',)
 """
 
-class UserForm(forms.ModelForm): 
+
+class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
-class UserProfileForm(forms.ModelForm):
 
+class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
 
+
 class BeerForm(forms.Form):
     title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the beer.")
-    #views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    #likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    #slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    # likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    # slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         fields = ('title')
+
 
 class CarrierForm(forms.Form):
     name = forms.CharField(max_length=128, help_text="Please enter the title of the beer carrier.")
